7b31a0a3cdb96962aeb433db084dd4ce2493afbe
Ham-JH <52693438+Ham-JH@users.noreply.github.com>
2020-09-19 17:43:36 +0900

    LooxidLink reference import

---
+++ Assets/02.Scripts/LooxidLinkDataController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LooxidLinkDataController : MonoBehaviour
+{
+    public Looxid.Link.LooxidLinkData looxidLinkData;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        looxidLinkData.GetEEGSensorStatus(0.2f);
+    }
+}
